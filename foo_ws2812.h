9c8216bb6dba8fa0f94094411365fac6631220b1
dr-boehmerie <Dr.Boehmerie@live.de>
2017-01-26 22:10:23 +0100

    removed unused playback_state.cpp; added BSD comments

--- foo_ws2812.h
+++ foo_ws2812.h
@@ -1,5 +1,31 @@
 #pragma once
 
+/*
+ * foo_ws2812.h : Defines all sorts of values, variables and functions.
+ *
+ * Copyright(c) 2016, 2017, Michael Boehme
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met :
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and / or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #define CALC_TAB_ELEMENTS(_TAB_)	(sizeof(_TAB_)/sizeof(_TAB_[0]))
 
 #define MAKE_COLOR(_R_,_G_,_B_)		((((_R_) & 0xFF) << 16) | (((_G_) & 0xFF) << 8) | ((_B_) & 0xFF))
--- mainmenu.cpp
+++ mainmenu.cpp
@@ -1,6 +1,33 @@
+/*
+ * mainmenu.cpp : Implentation of the visualisation menu entry.
+ *
+ * Based on mainmenu.cpp from the foo_sample plugin.
+ *
+ * Copyright(c) 2016, 2017, Michael Boehme
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met :
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and / or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.IN NO EVENT SHALL I BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "stdafx.h"
 
-//static const GUID g_mainmenu_group_id = { 0x44963e7a, 0x4b2a, 0x4588, { 0xb0, 0x17, 0xa8, 0x69, 0x18, 0xcb, 0x8a, 0xa5 } };
 // {064011FA-CFE4-4F47-A735-3FAE1D6FE5D3}
 static const GUID g_mainmenu_group_id = { 0x64011fa, 0xcfe4, 0x4f47,{ 0xa7, 0x35, 0x3f, 0xae, 0x1d, 0x6f, 0xe5, 0xd3 } };
 static const GUID g_mainmenu_entry_id = { 0x140c7ad4, 0x9176, 0x4afe,{ 0xa0, 0x31, 0x2f, 0x6b, 0xa3, 0x82, 0x42, 0x8c } };
@@ -17,37 +44,29 @@ static mainmenu_group_factory g_mainmenu_entry(g_mainmenu_entry_id, mainmenu_gro
 class mainmenu_group_commands_ws2812 : public mainmenu_commands {
 public:
 	enum {
-		cmd_playbackstate = 0,
-		cmd_show_control,
+		cmd_show_control = 0,
 		cmd_total
 	};
 	t_uint32 get_command_count() {
 		return cmd_total;
 	}
 	GUID get_command(t_uint32 p_index) {
-		//static const GUID guid_toggle_output = { 0x7c4726df, 0x3b2d, 0x4c7c, { 0xad, 0xe8, 0x43, 0xd8, 0x46, 0xbe, 0xce, 0xa8 } };
 		// {9D87536E-F82A-4613-8ED2-820C06546B63}
 		static const GUID guid_show_control = { 0x9d87536e, 0xf82a, 0x4613,{ 0x8e, 0xd2, 0x82, 0xc, 0x6, 0x54, 0x6b, 0x63 } };
-		//static const GUID guid_playbackstate = { 0xbd880c51, 0xf0cc, 0x473f, { 0x9d, 0x14, 0xa6, 0x6e, 0x8c, 0xed, 0x25, 0xae } };
-		// {BCF3A4A8-9436-45C6-AC75-420003C0D965}
-		static const GUID guid_playbackstate = { 0xbcf3a4a8, 0x9436, 0x45c6,{ 0xac, 0x75, 0x42, 0x0, 0x3, 0xc0, 0xd9, 0x65 } };
 
 		switch(p_index) {
-			case cmd_playbackstate: return guid_playbackstate;
 			case cmd_show_control: return guid_show_control;
 			default: uBugCheck(); // should never happen unless somebody called us with invalid parameters - bail
 		}
 	}
 	void get_name(t_uint32 p_index,pfc::string_base & p_out) {
 		switch(p_index) {
-			case cmd_playbackstate: p_out = "Playback state demo"; break;
 			case cmd_show_control: p_out = "Control dialog"; break;
 			default: uBugCheck(); // should never happen unless somebody called us with invalid parameters - bail
 		}
 	}
 	bool get_description(t_uint32 p_index,pfc::string_base & p_out) {
 		switch(p_index) {
-			case cmd_playbackstate: p_out = "Opens the playback state demo dialog."; return true;
 			case cmd_show_control: p_out = "Opens the control dialog."; return true;
 			default: uBugCheck(); // should never happen unless somebody called us with invalid parameters - bail
 		}
@@ -58,12 +77,7 @@ public:
 	}
 	void execute(t_uint32 p_index,service_ptr_t<service_base> p_callback) {
 		switch(p_index) {
-			case cmd_playbackstate:
-				RunPlaybackStateDemo();
-				break;
 			case cmd_show_control:
-			//	popup_message::g_show("This is a sample menu command.", "Blah");
-			//	ToggleOutput();
 				RunWS2812ControlDialog();
 				break;
 			default:
@@ -82,12 +96,8 @@ public:
 		return cmd_total;
 	}
 	GUID get_command(t_uint32 p_index) {
-		//static const GUID guid_toggle_output = { 0x7c4726df, 0x3b2d, 0x4c7c, { 0xad, 0xe8, 0x43, 0xd8, 0x46, 0xbe, 0xce, 0xa8 } };
 		// {9D87536E-F82A-4613-8ED2-820C06546B63}
 		static const GUID guid_show_control = { 0x9d87536e, 0xf82a, 0x4613,{ 0x8e, 0xd2, 0x82, 0xc, 0x6, 0x54, 0x6b, 0x63 } };
-		//static const GUID guid_playbackstate = { 0xbd880c51, 0xf0cc, 0x473f, { 0x9d, 0x14, 0xa6, 0x6e, 0x8c, 0xed, 0x25, 0xae } };
-		// {BCF3A4A8-9436-45C6-AC75-420003C0D965}
-		static const GUID guid_playbackstate = { 0xbcf3a4a8, 0x9436, 0x45c6,{ 0xac, 0x75, 0x42, 0x0, 0x3, 0xc0, 0xd9, 0x65 } };
 
 		switch (p_index) {
 		case cmd_show_control: return guid_show_control;
@@ -113,8 +123,6 @@ public:
 	void execute(t_uint32 p_index, service_ptr_t<service_base> p_callback) {
 		switch (p_index) {
 		case cmd_show_control:
-			//	popup_message::g_show("This is a sample menu command.", "Blah");
-			//	ToggleOutput();
 			RunWS2812ControlDialog();
 			break;
 		default:
